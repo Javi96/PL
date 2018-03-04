63d0b5d9a0b493d87ae964d25d53851a8ae1ae87
Pedro David <permuta.yt@gmail.com>
2018-03-05 00:53:29 +0100

    creada la implementacion manual

--- Manual/src/alex/AnalizadorLexicoTiny.java
+++ Manual/src/alex/AnalizadorLexicoTiny.java
@@ -1,9 +1,9 @@
 package alex;
 
 import java.io.FileInputStream;
-import java.io.Reader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class AnalizadorLexicoTiny {
 
@@ -17,8 +17,7 @@ public class AnalizadorLexicoTiny {
 	private static String NL = System.getProperty("line.separator");
 
 	private static enum Estado {
-		INICIO, MAS, MENOS, MULT, DIV, DIST_1, DIST_2, MENOR_TH, MENOR_EQ, MAYOR_TH, MAYOR_EQ, ASIG, EQ, END_L, PAP, PCI, AND_1, AND_2, NUM, DEC, NUM_DEC, EXP, NUM_EXP, EXP_MAS, EXP_MENOS, LETRA_1, LETRA_2;
-	}
+		INICIO, MAS, MENOS, MULT, DIV, DIST_1, DIST_2, MENOR_TH, MENOR_EQ, MAYOR_TH, MAYOR_EQ, ASIG, EQ, END_L, PAP, PCI, AND_1, AND_2, NUM, DEC, NUM_DEC, EXP, NUM_EXP, EXP_MAS, EXP_MENOS, LETRA_1, LETRA_2, REC_EOF;	}
 
 	private Estado estado;
 
@@ -38,135 +37,163 @@ public class AnalizadorLexicoTiny {
 		while (true) {
 			switch (estado) {
 			case INICIO:
-				if (hayLetra())
-					transita(Estado.LETRA_1);
-				else if (hayDigitoPos())
-					transita(Estado.NUM);
-				else if (haySuma())
+				if(haySuma()){
 					transita(Estado.MAS);
-				else if (hayResta())
+				}else if(hayResta()){
 					transita(Estado.MENOS);
-				else if (hayMul())
+				}else if(hayMul()){
 					transita(Estado.MULT);
-				else if (hayDiv())
+				}else if(hayDiv()){
 					transita(Estado.DIV);
-				else if (hayPAp())
-					transita(Estado.PAP);
-				else if (hayPCierre())
-					transita(Estado.PCI);
-				else if (hayIgual())
-					transita(Estado.ASIG);
-				else if (hayPuntoComa())
-					transita(Estado.END_L);
-				else if (hayExclmacion())
+				}else if(hayExcl()){
 					transita(Estado.DIST_1);
-				else if (hayMenor())
+				}else if(hayMenor()){
 					transita(Estado.MENOR_TH);
-				else if (hayMayor())
+				}else if(hayMayor()){
 					transita(Estado.MAYOR_TH);
-				else if (hayIgual())
+				}else if(hayIgual()){
 					transita(Estado.ASIG);
-				else if (hayAmpersand())
+				}else if(hayPuntoComa()){
+					transita(Estado.END_L);
+				}else if(hayPAp()){
+					transita(Estado.PAP);
+				}else if(hayPCierre()){
+					transita(Estado.PCI);
+				}else if(hayAmpersand()){
 					transita(Estado.AND_1);
-				else if (haySep())
-					transitaIgnorando(Estado.INICIO);
-				else
-					error();
-				break;
-			case LETRA_1:
-				if (hayLetra() || hayDigito() || haySubrayado())
-					transita(Estado.LETRA_2);
-				else
-					return unidadId();
-				break;
-			case NUM:
-				if (hayDigito())
+				}else if(hayDigito()){
 					transita(Estado.NUM);
-				else if (hayPunto())
-					transita(Estado.DEC);
-				else if(hayExp())
-					transita(Estado.EXP);
-				else
-					return unidadEnt();
+				}else if(hayLetra()){
+					transita(Estado.LETRA_1);
+				}else if(haySep()){
+					transitaIgnorando(Estado.INICIO);
+				}else if(hayEOF()){
+					transita(Estado.REC_EOF);
+				}else error();
 				break;
 			case MAS:
-				if (hayDigito())
-					transita(Estado.NUM);
-				else
-					return unidadEnt();
-				break;
-			case MENOS:
-				if (hayDigito())
+				if(hayDigito())
 					transita(Estado.NUM);
 				else
 					return unidadMas();
 				break;
-			case DIST_1:
-				if (hayIgual())
-					transita(Estado.DIST_2);
+			
+			case MENOS:
+				if(hayDigito())
+					transita(Estado.NUM);
 				else
-					return unidadDist();
+					return unidadMenos();
+				break;
+			case MULT:
+				return unidadPor();
+			
+			case DIV:
+				return unidadDiv();
+			
+			case DIST_1:
+				if(hayIgual())
+					transita(Estado.DIST_2);
 				break;
 			case MENOR_TH:
 				if(hayIgual())
 					transita(Estado.MENOR_EQ);
 				else
-					return unidadPor();
+					return unidadMenor();
+				break;
 			case MAYOR_TH:
 				if(hayIgual())
 					transita(Estado.MAYOR_EQ);
 				else
-					return unidaMayorTh();
+					return unidadMayor();
+				break;
 			case ASIG:
 				if(hayIgual())
 					transita(Estado.EQ);
 				else
-					return unidadEq();
+					return unidadIgual();
+				break;
+			case END_L:
+				return unidadPuntoComa();
+			case PAP:
+				return unidadPAp();
+			case PCI:
+				return unidadPCierre();
 			case AND_1:
 				if(hayAmpersand())
 					transita(Estado.AND_2);
+				break;
+			case LETRA_1:
+				if(hayLetra())
+					transita(Estado.LETRA_2);
 				else
-					return error();
+					return unidadId();
+				break;
+			case NUM:
+				if(hayDigito())
+					transita(Estado.NUM);
+				else if(hayPunto())
+					transita(Estado.DEC);
+				else if(hayExponente())
+					transita(Estado.EXP);
+				else
+					return unidadEnt();
+				break;
+			case DIST_2:
+				return unidadDistinto();
+			case MENOR_EQ:
+				return unidadMenorIgual();
+			case MAYOR_EQ:
+				return unidadMayorIgual();
+			case EQ:
+				return unidadIgualIgual();
+			case AND_2:
+				return unidadAmpersand();
+			case LETRA_2:
+				if(hayLetra()||hayDigito()||haySeparador())
+					transita(Estado.LETRA_2);
+				else
+					return unidadId();
+				break;
 			case DEC:
 				if(hayDigito())
 					transita(Estado.NUM_DEC);
-			case NUM_DEC:
-				if(hayExp())
-					transita(Estado.EXP);
-				
+				break;
 			case EXP:
-				if(hayMas())
+				if(hayDigito())
+					transita(Estado.NUM_EXP);
+				else if(haySuma())
 					transita(Estado.EXP_MAS);
-				else if(hayMenos())
+				else if(hayResta())
 					transita(Estado.EXP_MENOS);
-				else if(hayDigito())
+				break;
+			case EXP_MAS:
+				if(hayDigito())
+					transita(Estado.NUM_EXP);
+				break;
+			case EXP_MENOS:
+				if(hayDigito())
 					transita(Estado.NUM_EXP);
 				break;
 			case NUM_DEC:
 				if(hayDigito())
 					transita(Estado.NUM_DEC);
+				else if(hayExponente())
+					transita(Estado.EXP);
 				else
-					return unidadNumDec();
-			case EXP_MAS:
-				if (hayDigito())
-					transita(Estado.NUM_EXP);
-				else
-					error();
-				break;
-				
-			case EXP_MENOS:
-				if (hayDigito())
-					transita(Estado.NUM_EXP);
-				else
-					error();
+					return unidadReal();
 				break;
 			case NUM_EXP:
-				if (hayDigito())
+				if(hayDigito())
 					transita(Estado.NUM_EXP);
 				else
-					return unidadNumExp();
+					return unidadExp();
 				break;
-		}
+			case REC_EOF:
+				return unidadEof();
+			default:
+				break;
+			}
+		}	
 	}
 
 	private void transita(Estado sig) throws IOException {
@@ -263,21 +290,47 @@ public class AnalizadorLexicoTiny {
 		return sigCar == ' ' || sigCar == '\t' || sigCar == '\n';
 	}
 
-	private boolean hayNL() {
-		return sigCar == '\r' || sigCar == '\b' || sigCar == '\n';
-	}
-
 	private boolean hayEOF() {
 		return sigCar == -1;
+	}
+	
+	private boolean hayAmpersand() {
+
+		return sigCar == '&';
+	}
+	
+	private boolean haySeparador() {
+
+		return sigCar == '_';
+	}
+
+	private boolean hayMayor() {
+
+		return sigCar == '>';
+	}
+
+	private boolean hayMenor() {
+
+		return sigCar == '<';
+	}
+
+	private boolean hayExcl() {
+
+		return sigCar == '!';
 	}
 
 	private UnidadLexica unidadId() {
 		// palabras reservadas
 		switch (lex.toString()) {
-		case "evalua":
-			return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.EVALUA);
-		case "donde":
-			return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.DONDE);
+		case "true":
+			return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.TRUE);
+		case "false":
+			return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.FALSE);
+		case "num":
+			return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.NUM);
+		case "bool":
+			return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.BOOL);
+
 		default:
 			return new UnidadLexicaMultivaluada(filaInicio, columnaInicio, ClaseLexica.IDEN, lex.toString());
 		}
@@ -318,15 +371,56 @@ public class AnalizadorLexicoTiny {
 	private UnidadLexica unidadIgual() {
 		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.IGUAL);
 	}
+	
+	private UnidadLexica unidadExp() {
 
-	private UnidadLexica unidadComa() {
-		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.COMA);
+		return new UnidadLexicaMultivaluada(filaInicio, columnaInicio, ClaseLexica.EXP, lex.toString());
+	}
+
+	private UnidadLexica unidadAmpersand() {
+
+		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.AMPERSAND);
+	}
+
+	private UnidadLexica unidadIgualIgual() {
+
+		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.IGUALIGUAL);
+	}
+
+	private UnidadLexica unidadMayorIgual() {
+
+		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.MAYORIGUAL);
+	}
+
+	private UnidadLexica unidadMenorIgual() {
+
+		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.MENORIGUAL);
+	}
+
+	private UnidadLexica unidadDistinto() {
+
+		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.DISTINTO);
+	}
+
+	private UnidadLexica unidadPuntoComa() {
+
+		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.PUNTOCOMA);
+	}
+
+	private UnidadLexica unidadMayor() {
+
+		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.MAYOR);
+	}
+	
+	private UnidadLexica unidadMenor() {
+
+		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.MENOR);
 	}
 
 	private UnidadLexica unidadEof() {
-		return new UnidadLexicaUnivaluada(filaInicio, columnaInicio, ClaseLexica.EOF);
-	}
-
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.EOF);     
+	}  
+	
 	private void error() {
 		System.err.println("(" + filaActual + ',' + columnaActual + "):Caracter inexperado");
 		System.exit(1);
